79e9a6c031b8685f0a42a9797135802ee4aa0196
FeliciaChoo <147573086+FeliciaChoo@users.noreply.github.com>
2025-05-22 14:09:14 +0800

    project page done

--- src/main/java/com/example/projectEdu/repository/ProjectRepository.java
+++ src/main/java/com/example/projectEdu/repository/ProjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,4 +25,6 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     Integer countByStudentIdAndStatus(Long id, String status);
 
     List<Project> findUpcomingProjects();
+
+    List<Project> findByStatusAndEndDateAfter(String active, LocalDate now);
 }
